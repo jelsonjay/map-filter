e125c6a84c10e5d706bd92a374ae0ee15167e029
Jelson J <jelson20outlook.com>
2020-11-21 16:34:47 +0000

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,17 +1,22 @@
 'strict'
-
+// staff list
 const staffList = [
   {name: 'Jackson P', age: 35, occupation: 'design', gender: 'female'},
   {name: 'Jennifer Smith', age: 19, occupation: 'full-stack', gender: 'female'},
   {name: 'James Parker', age: 22, occupation: 'front-end', gender: 'male'},
-  {name: 'Lopes T', age: 47, occupation: 'developer', gender: 'male'},
+  {name: 'Lopes T', age: 47, occupation: 'web developer', gender: 'male'},
   {name: 'Emma Harry', age: 17, occupation: 'developer', gender: 'female'},
   {name: 'Jason Tyson', age: 28, occupation: 'tester', gender: 'male'},
   {name: 'Luke Jame', age: 33, occupation: 'design', gender: 'male'},
   {name: 'Rosa Batista', age: 50, occupation: 'account', gender: 'female'},
   {name: 'Lopes T', age: 19, occupation: 'developer', gender: 'male'},
-  {name: 'Ruth Lordes', age: 20, occupation: 'tester', gender: 'female'}
+  {name: 'Leila White', age: 16, occupation: 'tester', gender: 'female'},
+  {name: 'Les Sugar', age: 39, occupation: 'front-end', gender: 'male'},
+  {name: 'Julian Brown', age: 26, occupation: 'tester', gender: 'female'},
+  {name: 'Andy Per', age: 18, occupation: 'full-stack', gender: 'male'}
 ]
+
+staffList.push({name: 'Andre', age: 16, occupation: 'junior developer', gender: 'male'})
 
 const emp = staffList.map(onlyName)
 
@@ -26,15 +31,17 @@ function onlyName(e){
 }
 
 function onlyAges(e){
-return e.age < 3
+return e.age < 20
 }
 
 let youngStaff = staffList.filter(onlyAges).filter(onlyFemale).map(onlyName)
 
 console.log('-------------all employes list----------------')
+console.log(staffList)
+console.log('-------------only staff name----------------')
 console.log(emp)
 console.log('-------------only female-----------------')
 console.log(female)
 
-console.log('------------------------------')
-console.log(female)
+console.log('-------------only staff age under 20 year ages-----------------')
+console.log(youngStaff)
